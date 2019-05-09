28f2353e880253b056e404a1f2de44884ef37829
Will Jones <willjones2010@gmail.com>
2019-05-09 15:08:16 -0400

    menu progressed. random quotes work 2 of 4 menu options working

--- app/gv_controller.rb
+++ app/gv_controller.rb
@@ -10,15 +10,30 @@ class GVController
       puts "Good Vibes Menu"
       puts "1. Random Motivational quotes"
       puts "2. Favorite quotes"
+      puts "3. Remove a favorite quote : ("
       puts "4. Exit"
 
   choice = gets.chomp
 
   case (choice.to_i)
     when 1
-      $user.random_quote
+      random_quote = $user.random_quote
+      puts random_quote
+      puts "Would you like to favorite this quote? yes or no."
+        answer1 = gets.chomp
+        if answer1 == 'yes'
+          $user.favorite_a_quote(random_quote)
+        elsif answer1 == 'no'
+          main_menu
+        else
+          "Thats not an option"
+        end
     when 2
-      $user.see_my_favorites
+      puts $user.see_my_favorites
+    when 3
+      id_to_get_deleted = gets.chomp.to_i
+      $user.delete_a_favorite(id_to_get_deleted)
+      puts $user.see_my_favorites
     when 4
       exit
   else
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :quotes, through: :favorites
 
   def random_quote
-    Quote.all.sample
+    puts Quote.all.sample.quote_text
   end
 
   def favorite_a_quote(id)
