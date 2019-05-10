290236a6a4274b8122832341881f77b23049ff9e
Will Jones <willjones2010@gmail.com>
2019-05-09 23:04:41 -0400

    added in quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,13 @@ User.delete_all
 Quote.delete_all
 Favorite.delete_all
 
-will = User.create(name: "Will")
-blake = User.create(name: "Blake")
-cool_quote = Quote.create(quote_text: "This is a super duper quote a moat!")
-new_quote = Quote.create(quote_text: "Shazambam is the coolest quote thats ever been quoted")
-# new_fav = Favorite.create(user_id: User.last.id, quote_id: Quote.last.id)
+Quote.create(quote_text: "Believe you can and you're halfway there.")
+Quote.create(quote_text: "Be more concerned with your character than with your reputation. Your character is what you really are, while your reputation is merely what others think you are.")
+Quote.create(quote_text: "Change your thoughts and you change your world.")
+Quote.create(quote_text: "Success is never final. Failure is never fatal. It is courage that counts.")
+Quote.create(quote_text: "Edison failed 10, 000 times before he made the electric light. Do not be discouraged if you fail a few times.")
+Quote.create(quote_text: "Out of clutter, find Simplicity. From discord, find Harmony. In the middle of difficulty lies Opportunity.")
+Quote.create(quote_text: "Belief creates the actual fact.")
+Quote.create(quote_text: "Don't judge each day by the harvest you reap but by the seeds that you plant."
+Quote.create(quote_text: "The best way out is always through.")
+Quote.create(quote_text: "With self-discipline most anything is possible.")
